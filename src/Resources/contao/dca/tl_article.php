bf15f94f5979e1f2497271dc74b6642288d853c5
websailing <info@websailing.de>
2025-11-29 11:47:58 +0100

    Contao 5.3.42

--- src/Resources/contao/dca/tl_article.php
+++ src/Resources/contao/dca/tl_article.php
@@ -2,4 +2,4 @@
 
 // Add the ID of articles to the list view label.
 $GLOBALS['TL_DCA']['tl_article']['list']['label']['fields'][] = 'id';
-$GLOBALS['TL_DCA']['tl_article']['list']['label']['format']= '%s <span class="label-info">[%s, ID: %s]</span>';
+$GLOBALS['TL_DCA']['tl_article']['list']['label']['format']= '%s <span class="subtle">[%s, ID: %s]</span>';
